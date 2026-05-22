026bc176e5db848e268d3f5e69571bf612c87561
yashturkar <yashturkar@gmail.com>
2026-05-22 09:13:31 -0400

    Add D-pad flashlight aim controls

--- examples/flashlight/run.py
+++ examples/flashlight/run.py
@@ -41,9 +41,25 @@ parser.add_argument("--disable-scene-lights", action="store_true")
 parser.add_argument("--capture-poses", action="store_true")
 parser.add_argument("--capture-key", default="Gamepad_FaceButton_Top")
 parser.add_argument("--toggle-key", default="Gamepad_FaceButton_Right")
+parser.add_argument("--aim-yaw-min-degrees", type=float, default=-30.0)
+parser.add_argument("--aim-yaw-max-degrees", type=float, default=30.0)
+parser.add_argument("--aim-pitch-min-degrees", type=float, default=-30.0)
+parser.add_argument("--aim-pitch-max-degrees", type=float, default=30.0)
+parser.add_argument("--aim-rate-degrees-per-second", type=float, default=90.0)
+parser.add_argument("--aim-left-key", default="Gamepad_DPad_Left")
+parser.add_argument("--aim-right-key", default="Gamepad_DPad_Right")
+parser.add_argument("--aim-up-key", default="Gamepad_DPad_Up")
+parser.add_argument("--aim-down-key", default="Gamepad_DPad_Down")
 parser.add_argument("--pose-output-file", default=os.path.realpath(os.path.join(os.path.dirname(__file__), "camera_poses.jsonl")))
 parser.add_argument("--idle-period-seconds", type=float, default=0.5)
 args = parser.parse_args()
+
+if args.aim_yaw_min_degrees > args.aim_yaw_max_degrees:
+    parser.error("--aim-yaw-min-degrees must be less than or equal to --aim-yaw-max-degrees")
+if args.aim_pitch_min_degrees > args.aim_pitch_max_degrees:
+    parser.error("--aim-pitch-min-degrees must be less than or equal to --aim-pitch-max-degrees")
+if args.aim_rate_degrees_per_second < 0.0:
+    parser.error("--aim-rate-degrees-per-second must be non-negative")
 
 
 def get_viewport_pose(game):
@@ -56,6 +72,24 @@ def set_light_pose(light, viewport_desc):
         NewRotation=viewport_desc["camera_rotation"],
         bSweep=False,
         bTeleport=True)
+
+
+def get_aimed_light_rotation(camera_rotation, yaw_offset_degrees, pitch_offset_degrees):
+    light_rotation = to_plain_dict(camera_rotation)
+    rotator_key_by_lower_name = {key.lower(): key for key in light_rotation.keys()}
+    yaw_key = rotator_key_by_lower_name["yaw"]
+    pitch_key = rotator_key_by_lower_name["pitch"]
+    light_rotation[yaw_key] += yaw_offset_degrees
+    light_rotation[pitch_key] += pitch_offset_degrees
+    return light_rotation
+
+
+def set_light_aim(light, viewport_desc, yaw_offset_degrees, pitch_offset_degrees):
+    return light.K2_SetActorRotation(
+        NewRotation=get_aimed_light_rotation(
+            camera_rotation=viewport_desc["camera_rotation"],
+            yaw_offset_degrees=yaw_offset_degrees,
+            pitch_offset_degrees=pitch_offset_degrees))
 
 
 def to_plain_dict(value):
@@ -83,6 +117,10 @@ def get_input_key_arg(key_name):
 
 def is_input_key_down(player_controller, key_name):
     return player_controller.IsInputKeyDown(Key=get_input_key_arg(key_name=key_name))
+
+
+def clamp(value, min_value, max_value):
+    return min(max(value, min_value), max_value)
 
 
 def was_input_key_pressed_since_last_poll(player_controller, key_name, previous_key_down_by_name):
@@ -188,6 +226,20 @@ if __name__ == "__main__":
         spear.log("Spawned camera flashlight. Press Ctrl+C to stop.")
         spear.log("Camera movement speed: ", args.movement_speed)
         spear.log("Flashlight toggle key: ", args.toggle_key)
+        spear.log("Flashlight aim D-pad keys: ", args.aim_left_key, args.aim_right_key, args.aim_up_key, args.aim_down_key)
+        spear.log(
+            "Flashlight aim yaw range: ",
+            args.aim_yaw_min_degrees,
+            " to ",
+            args.aim_yaw_max_degrees,
+            " degrees")
+        spear.log(
+            "Flashlight aim pitch range: ",
+            args.aim_pitch_min_degrees,
+            " to ",
+            args.aim_pitch_max_degrees,
+            " degrees")
+        spear.log("Flashlight aim rate: ", args.aim_rate_degrees_per_second, " degrees per second")
         if args.capture_poses:
             spear.log("Pose capture enabled. Press Enter in this terminal or press the capture key on the controller to save the current camera pose.")
             spear.log("Capture key: ", args.capture_key)
@@ -195,11 +247,19 @@ if __name__ == "__main__":
 
         pose_index = 0
         flashlight_visible = True
+        aim_yaw_offset_degrees = 0.0
+        aim_pitch_offset_degrees = 0.0
+        previous_poll_time = time.monotonic()
         previous_key_down_by_name = {}
         while instance.is_running():
             time.sleep(min(args.idle_period_seconds, INPUT_POLL_PERIOD_SECONDS))
+            poll_time = time.monotonic()
+            poll_delta_seconds = poll_time - previous_poll_time
+            previous_poll_time = poll_time
             should_capture_pose = False
             should_toggle_flashlight = False
+            aim_yaw_direction = 0.0
+            aim_pitch_direction = 0.0
             if args.capture_poses:
                 if sys.stdin in select.select([sys.stdin], [], [], 0.0)[0]:
                     sys.stdin.readline()
@@ -216,8 +276,35 @@ if __name__ == "__main__":
                         player_controller=player_controller,
                         key_name=args.capture_key,
                         previous_key_down_by_name=previous_key_down_by_name)
+                if is_input_key_down(player_controller=player_controller, key_name=args.aim_left_key):
+                    aim_yaw_direction -= 1.0
+                if is_input_key_down(player_controller=player_controller, key_name=args.aim_right_key):
+                    aim_yaw_direction += 1.0
+                if is_input_key_down(player_controller=player_controller, key_name=args.aim_up_key):
+                    aim_pitch_direction += 1.0
+                if is_input_key_down(player_controller=player_controller, key_name=args.aim_down_key):
+                    aim_pitch_direction -= 1.0
             with instance.end_frame():
                 pass
+
+            if aim_yaw_direction != 0.0 or aim_pitch_direction != 0.0:
+                aim_yaw_offset_degrees = clamp(
+                    value=aim_yaw_offset_degrees + aim_yaw_direction * args.aim_rate_degrees_per_second * poll_delta_seconds,
+                    min_value=args.aim_yaw_min_degrees,
+                    max_value=args.aim_yaw_max_degrees)
+                aim_pitch_offset_degrees = clamp(
+                    value=aim_pitch_offset_degrees + aim_pitch_direction * args.aim_rate_degrees_per_second * poll_delta_seconds,
+                    min_value=args.aim_pitch_min_degrees,
+                    max_value=args.aim_pitch_max_degrees)
+                with instance.begin_frame():
+                    viewport_desc = get_viewport_pose(game=game)
+                    set_light_aim(
+                        light=light,
+                        viewport_desc=viewport_desc,
+                        yaw_offset_degrees=aim_yaw_offset_degrees,
+                        pitch_offset_degrees=aim_pitch_offset_degrees)
+                with instance.end_frame():
+                    pass
 
             if should_toggle_flashlight:
                 flashlight_visible = not flashlight_visible
